085b137b8d4106d40f808ccbe01abd5d182738cb
ari337 <70830049+ari337@users.noreply.github.com>
2023-05-11 17:01:41 +0900

    구름까지만들기
    구름까지만들기

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
+    Animator animator;
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rigid2D=GetComponent<Rigidbody2D>();
+        this.animator=GetComponent<Animator>();
     }
     private void Update()
     {
@@ -38,6 +40,8 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(key, 1, 1);
         }
-        
+
+        //플레이어 속도에 맞춰 애니메이션 속도를 바꾼다.
+        animator.speed = (float)(speedx / 2.0);
     }
 }
